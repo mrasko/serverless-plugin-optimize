5c55344429e6a1e90c178037cd6f5b0aa2f85381
Marko <52242358+mrasko@users.noreply.github.com>
2024-02-14 11:22:35 +0100

    Add node 16 and 18 support

--- src/index.js
+++ src/index.js
@@ -46,7 +46,9 @@ class Optimize {
       this.serverless.service.provider.runtime === 'nodejs8.10' ||
       this.serverless.service.provider.runtime === 'nodejs10.x' ||
       this.serverless.service.provider.runtime === 'nodejs12.x' ||
-      this.serverless.service.provider.runtime === 'nodejs14.x')
+      this.serverless.service.provider.runtime === 'nodejs14.x' ||
+      this.serverless.service.provider.runtime === 'nodejs16.x' ||
+      this.serverless.service.provider.runtime === 'nodejs18.x')
 
     /** AWS provider and valid runtime check */
     if (validRunTime) {
